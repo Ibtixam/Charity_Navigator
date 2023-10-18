e45ad97fc412c5b0d530330c296d3ccf16fc8624
M. Ibtisam <ibtisamkhan524@gmail.com>
2023-10-18 18:41:10 +0500

    Remove some functions

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Entries from "./components/entries";
 
 const App = () => {
-  const getScreenInfo = () => {
-    const { innerWidth: width, innerHeight: height } = window;
-    document.body.style.setProperty("--width", `${width}px`);
-    document.body.style.setProperty("--height", `${height}px`);
-    window.scrollTo(0, 0);
-  };
-  window.addEventListener("resize", getScreenInfo);
-  window.addEventListener("orientationchange", getScreenInfo);
   return (
     <React.Fragment>
       <Router>
